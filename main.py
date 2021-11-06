ee43eee15e73271f2b86be65072f86f4f22fb5fe
bonecrunch5 <goncalo.valpneus@hotmail.com>
2021-11-06 15:59:37 +0000

    Initial keys

--- main.py
+++ main.py
@@ -52,15 +52,15 @@ while(True):
 
     # Draw countours in original image
     image_copy = img.copy()
-    
+
     mask4 = np.zeros_like(image_copy)
     mask4 = cv2.cvtColor(mask4, cv2.COLOR_BGR2GRAY)
     #mask[contours > 0.01*contours.max()] = 255
-    
+
     for contour in contours:
         if cv2.contourArea(contour) > 10000:
             cv2.drawContours(image=mask4, contours=contour, contourIdx=-1,
-                     color=(255, 255, 255), thickness=2, lineType=cv2.LINE_8)
+                             color=(255, 255, 255), thickness=2, lineType=cv2.LINE_8)
             perimeter = cv2.arcLength(contour, True)
             approx = cv2.approxPolyDP(contour, 0.05 * perimeter, True)
 
@@ -72,35 +72,60 @@ while(True):
             cv2.drawContours(image_copy, [approx], -1, (0, 255, 0))
 
             if(len(approx) == 4):
-                pts_dst = np.array([[792,1],[792, 380],[1, 380],[1,1]])
+                pts_dst = np.array([[792, 1], [792, 380], [1, 380], [1, 1]])
                 h, status = cv2.findHomography(approx, pts_dst)
 
-                # im_dst = cv2.imread('book2.jpg')
-                # im_out = cv2.warpPerspective(img, h, (im_dst.shape[1],im_dst.shape[0]))
-                
                 # Hardcoded proportions to be changed in the future
                 im_out = cv2.warpPerspective(img, h, (792, 380))
                 cv2.imshow("Warped Source Image", im_out)
 
-    """ cv2.drawContours(image=image_copy, contours=contours, contourIdx=-1,
-                     color=(0, 255, 0), thickness=2, lineType=cv2.LINE_AA) """
+                # Convert from RGB to grayscale
+                imgGray_out = cv2.cvtColor(im_out, cv2.COLOR_BGR2GRAY)
 
-    ########################################################################## does not work     Approximate contours with linear features (cvApproxPoly)
+                # Smoothing
+                imgSmooth_out = cv2.blur(imgGray_out, (5, 5))
 
-    # epsilon = 0.1 * cv2.arcLength(contours[0], True)
-    # approx = cv2.approxPolyDP(contours[0], epsilon, True)
+                # Threshold
+                (T, imgThresh_out) = cv2.threshold(
+                    imgSmooth_out, 120, 255, cv2.THRESH_BINARY)
 
-    # image_copy2 = img.copy()
+                # Find Countours
+                imgCanny_out = cv2.Canny(imgThresh_out, 100, 200)
 
-    # cv2.drawContours(image_copy2, approx, -1, (0, 255, 0), 10)
-    ##########################################################################
+                cv2.imshow('img canny out', imgCanny_out)
+
+                contours_out, hierarchy_out = cv2.findContours(
+                    imgCanny_out, cv2.RETR_TREE, cv2.CHAIN_APPROX_NONE)
+
+                im_out_copy = im_out.copy()
+                mask_out = np.zeros_like(im_out_copy)
+                mask_out = cv2.cvtColor(mask_out, cv2.COLOR_BGR2GRAY)
+
+                for contour_out in contours_out:
+                    if cv2.contourArea(contour_out) > 700:
+                        cv2.drawContours(image=mask_out, contours=contour_out, contourIdx=-1,
+                             color=(255, 255, 255), thickness=2, lineType=cv2.LINE_8)
+                        perimeter_out = cv2.arcLength(contour_out, True)
+                        approx_out = cv2.approxPolyDP(contour_out, 0.05 * perimeter_out, True)
+
+                        for point_out in approx_out:
+                            x, y = point_out[0]
+                            cv2.circle(im_out_copy, (x, y), 3, (0, 255, 0), -1)
+
+                # dst_out = cv2.cornerHarris(imgSmooth_out, 2, 3, 0.04)
+
+                # im_out_copy = im_out.copy()
+
+                # mask_out = np.zeros_like(im_out_copy)
+                # mask_out[dst_out > 0.01*dst_out.max()] = 255
+                        cv2.imshow('mask_out', mask_out)
 
     image_copy3 = img.copy()
     image_copy4 = img.copy()
 
     dst = cv2.cornerHarris(imgGray, 2, 3, 0.04)
     dst2 = cv2.cornerHarris(imgSmooth, 2, 3, 0.04)
-    
+
     # --- create a black image to see where those corners occur ---
     mask = np.zeros_like(image_copy3)
     mask2 = np.zeros_like(image_copy3)
@@ -109,8 +134,8 @@ while(True):
     mask[dst > 0.01*dst.max()] = 255
     mask2[dst2 > 0.01*dst2.max()] = 255
 
-    image_copy3[dst > 0.01 * dst.max()] = [0, 0, 255]  
-    image_copy4[dst2 > 0.01 * dst2.max()] = [0, 0, 255]  
+    image_copy3[dst > 0.01 * dst.max()] = [0, 0, 255]
+    image_copy4[dst2 > 0.01 * dst2.max()] = [0, 0, 255]
 
     #corners = cv2.goodFeaturesToTrack(mask4, 10000, 0.3, 100)
     corners = cv2.cornerHarris(mask4, 2, 3, 0.04)
@@ -128,24 +153,24 @@ while(True):
             cv2.circle(image_copy3, (int(corners[i,0,0]), int(corners[i,0,1])), 7, (0,255,0), 2)
     """
     # Display the resulting frame
-    #cv2.imshow('img',img)
+    cv2.imshow('img',img)
     #cv2.imshow('img gray',imgGray)
     #cv2.imshow('img smooth',imgSmooth)
     #cv2.imshow('img threshold',imgThresh)
-    cv2.imshow('img canny', imgCanny)
-    cv2.imshow('img copy', image_copy)
-    # cv2.imshow('img copy 2', image_copy2)
+    #cv2.imshow('img canny', imgCanny)
+    #cv2.imshow('img copy', image_copy)
+    #cv2.imshow('img copy 2', image_copy2)
     #cv2.imshow('img copy 3', image_copy3)
-    cv2.imshow('img copy 4', image_copy4)
+    #cv2.imshow('img copy 4', image_copy4)
     #cv2.imshow('mask', mask)
-    cv2.imshow('mask2', mask2)
-    cv2.imshow('mask4', mask4)
+    #cv2.imshow('mask2', mask2)
+    #cv2.imshow('mask4', mask4)
 
     # cv2.imshow('img copy 4', image_copy4)
 
     # Waits for a user input to quit the application
     if cv2.waitKey(1) & 0xFF == ord('q'):
-        #print(contours)
+        # print(contours)
 
         break
 
