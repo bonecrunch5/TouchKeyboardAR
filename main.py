f7e23834c3e491185cce9bcd1648337a6fbae0c6
bonecrunch5 <goncalo.valpneus@hotmail.com>
2021-11-09 15:18:20 +0000

    Code Refactor Co-authored-by: Filipe Barbosa <Octanas@users.noreply.github.com> Co-authored-by: João Ricardo Cardoso <JoaoRicardoCardoso@users.noreply.github.com>

--- main.py
+++ main.py
@@ -5,12 +5,16 @@ import json
 from dotenv import load_dotenv
 load_dotenv()
 
-def reorder(points):
-    result = np.array([[[0,0]],[[0,0]],[[0,0]],[[0,0]]], ndmin=3)
 
-    mediumPoint1 = [(points[0][0][0] + points[2][0][0])/2, (points[0][0][1] + points[2][0][1])/2]
-    mediumPoint2 = [(points[1][0][0] + points[3][0][0])/2, (points[1][0][1] + points[3][0][1])/2]
-    averageMediumPoint = [(mediumPoint1[0] + mediumPoint2[0])/2, (mediumPoint1[1] + mediumPoint2[1])/2]
+def reorder(points):  # TODO might have to redo this
+    result = np.array([[[0, 0]], [[0, 0]], [[0, 0]], [[0, 0]]], ndmin=3)
+
+    mediumPoint1 = [(points[0][0][0] + points[2][0][0])/2,
+                    (points[0][0][1] + points[2][0][1])/2]
+    mediumPoint2 = [(points[1][0][0] + points[3][0][0])/2,
+                    (points[1][0][1] + points[3][0][1])/2]
+    averageMediumPoint = [(mediumPoint1[0] + mediumPoint2[0])/2,
+                          (mediumPoint1[1] + mediumPoint2[1])/2]
 
     for point in points:
         if point[0][0] > averageMediumPoint[0] and point[0][1] < averageMediumPoint[1]:
@@ -35,6 +39,9 @@ cap = cv2.VideoCapture(int(os.environ.get('CAMERA_ID', '0')))
 
 if not (cap.isOpened()):
     print('Could not open video device')
+    cap.release()
+    cv2.destroyAllWindows()
+    exit()
 
 # To set the resolution
 cap.set(cv2.CAP_PROP_FRAME_WIDTH, 1000)
@@ -59,24 +66,71 @@ cap.set(cv2.CAP_PROP_FRAME_HEIGHT, 1080)
  """
 jsonOutput = {}
 imgKeyboard = None
-while(True):
-    # Capture frame-by-frame
-    success, img = cap.read()
 
+
+def getContours(image):
     # Convert from RGB to grayscale
-    imgGray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+    imgGray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
 
     # Smoothing
     imgSmooth = cv2.blur(imgGray, (5, 5))
 
     # Threshold
-    (T, imgThresh) = cv2.threshold(imgSmooth, 100, 255, cv2.THRESH_BINARY) #TODO adjust values of threshold
+    (_, imgThresh) = cv2.threshold(imgSmooth, 100, 255,
+                                   cv2.THRESH_BINARY)  # TODO adjust values of threshold
 
     # Find Countours
     imgCanny = cv2.Canny(imgThresh, 100, 200)
 
-    contours, hierarchy = cv2.findContours(
+    contours, _ = cv2.findContours(
         imgCanny, cv2.RETR_TREE, cv2.CHAIN_APPROX_NONE)
+
+    return contours
+
+
+def processKeys(contours_out):
+    counter = 0
+    jsonObject = {"keys": []}
+    for contour_out in contours_out:
+        if cv2.contourArea(contour_out, True) > 700:
+            counter += 1
+            cv2.drawContours(image=mask_out, contours=contour_out, contourIdx=-1,
+                             color=(255, 255, 255), thickness=2, lineType=cv2.LINE_8)
+            perimeter_out = cv2.arcLength(contour_out, True)
+            approx_out = cv2.approxPolyDP(
+                contour_out, 0.04 * perimeter_out, True)
+
+            jsonKey = []
+            for point_out in approx_out:
+                x, y = point_out[0]
+                cv2.circle(im_out_copy, (x, y), 3, (0, 255, 0), -1)
+
+                jsonKey.append([int(x), int(y)])
+
+            jsonObject['keys'].append(jsonKey)
+
+    cv2.imshow('im_out_copy', im_out_copy)
+    cv2.imshow('mask_out', mask_out)
+    print(counter)
+    return jsonObject
+
+
+def processHomography(approxPolygon):
+    approxPolygon = reorder(approxPolygon)
+    pts_dst = np.array([[792, 1], [792, 380], [1, 380], [1, 1]])
+    homographyMatrix, _ = cv2.findHomography(approxPolygon, pts_dst)
+
+    # Hardcoded proportions to be changed in the future
+    global imgKeyboard
+    imgKeyboard = cv2.warpPerspective(img, homographyMatrix, (792, 380))
+    cv2.imshow("Warped Source Image", imgKeyboard)
+
+
+while(True):
+    # Capture frame-by-frame
+    _, img = cap.read()
+
+    contours = getContours(img)
 
     # Draw countours in original image
     imageCopy = img.copy()
@@ -88,7 +142,7 @@ while(True):
     #mask[contours > 0.01*contours.max()] = 255
 
     for contour in contours:
-        if cv2.contourArea(contour,True) > 10000:
+        if cv2.contourArea(contour, True) > 10000:
             cv2.drawContours(image=blackImage, contours=contour, contourIdx=-1,
                              color=(255, 255, 255), thickness=2, lineType=cv2.LINE_8)
             perimeter = cv2.arcLength(contour, True)
@@ -102,98 +156,18 @@ while(True):
             cv2.drawContours(imageCopy, [approxPolygon], -1, (0, 255, 0))
 
             if(len(approxPolygon) == 4):
-                approxPolygon = reorder(approxPolygon)
-                pts_dst = np.array([[792, 1], [792, 380], [1, 380], [1, 1]])
-                h, status = cv2.findHomography(approxPolygon, pts_dst)
+                processHomography(approxPolygon)
+                contours_out = getContours(imgKeyboard)
 
-                # Hardcoded proportions to be changed in the future
-                im_out = cv2.warpPerspective(img, h, (792, 380))
-                cv2.imshow("Warped Source Image", im_out)
-                imgKeyboard = im_out.copy()
-                # Convert from RGB to grayscale
-                imgGray_out = cv2.cvtColor(im_out, cv2.COLOR_BGR2GRAY)
-
-                # Smoothing
-                imgSmooth_out = cv2.blur(imgGray_out, (5, 5))
-
-                # Threshold
-                (T, imgThresh_out) = cv2.threshold(
-                    imgSmooth_out, 120, 255, cv2.THRESH_BINARY)
-
-                # Find Countours
-                imgCanny_out = cv2.Canny(imgThresh_out, 100, 200)
-
-                cv2.imshow('img canny out', imgCanny_out)
-
-                contours_out, hierarchy_out = cv2.findContours(
-                    imgCanny_out, cv2.RETR_TREE, cv2.CHAIN_APPROX_NONE)
-
-                im_out_copy = im_out.copy()
+                im_out_copy = imgKeyboard.copy()
                 mask_out = np.zeros_like(im_out_copy)
                 mask_out = cv2.cvtColor(mask_out, cv2.COLOR_BGR2GRAY)
 
-                counter=0
-                jsonObject = {"keys":[]}
-                for contour_out in contours_out:
-                   
-                    if cv2.contourArea(contour_out, True) > 700:
-                        counter+=1
-                        cv2.drawContours(image=mask_out, contours=contour_out, contourIdx=-1,
-                             color=(255, 255, 255), thickness=2, lineType=cv2.LINE_8)
-                        perimeter_out = cv2.arcLength(contour_out, True)
-                        approx_out = cv2.approxPolyDP(contour_out, 0.04 * perimeter_out, True)
-                        
-                        jsonKey = []
-                        for point_out in approx_out:
-                            x, y = point_out[0]
-                            cv2.circle(im_out_copy, (x, y), 3, (0, 255, 0), -1)
-
-                            jsonKey.append([int(x),int(y)])
-
-                        jsonObject['keys'].append(jsonKey)
-
-                
-                        cv2.imshow('im_out_copy', im_out_copy)
-                        cv2.imshow('mask_out', mask_out)
-                print(counter)
+                jsonObject = processKeys(contours_out)
                 jsonOutput = json.dumps(jsonObject)
 
-                              
-
-    imageCopy3 = img.copy()
-    imageCopy4 = img.copy()
-
-    dst = cv2.cornerHarris(imgGray, 2, 3, 0.04)
-    dst2 = cv2.cornerHarris(imgSmooth, 2, 3, 0.04)
-
-    # --- create a black image to see where those corners occur ---
-    mask = np.zeros_like(imageCopy3)
-    mask2 = np.zeros_like(imageCopy4)
-
-    # --- applying a threshold and turning those pixels above the threshold to white ---
-    mask[dst > 0.01*dst.max()] = 255
-    mask2[dst2 > 0.01*dst2.max()] = 255
-
-    imageCopy3[dst > 0.01 * dst.max()] = [0, 0, 255]
-    imageCopy4[dst2 > 0.01 * dst2.max()] = [0, 0, 255]
-
-    #corners = cv2.goodFeaturesToTrack(mask4, 10000, 0.3, 100)
-    corners = cv2.cornerHarris(blackImage, 2, 3, 0.04)
-
-    # --- create a black image to see where those corners occur ---
-    # mask4 = np.zeros_like(imageCopy4)
-
-    # # --- applying a threshold and turning those pixels above the threshold to white ---
-    # mask4[dst > 0.01*dst.max()] = 255
-
-    # solvePNP
-    """ if corners is not None :
-        for i in range(1, len(corners)):
-            print(corners[i,0,0])
-            cv2.circle(imageCopy3, (int(corners[i,0,0]), int(corners[i,0,1])), 7, (0,255,0), 2)
-    """
     # Display the resulting frame
-    cv2.imshow('img',img)
+    cv2.imshow('img', img)
     #cv2.imshow('img gray',imgGray)
     #cv2.imshow('img smooth',imgSmooth)
     #cv2.imshow('img threshold',imgThresh)
@@ -220,12 +194,11 @@ while(True):
 # When everything done, release the capture
 if not os.path.exists("generated"):
     os.mkdir("generated")
-f=open("generated/keys.json","w")
+f = open("generated/keys.json", "w")
 f.write(jsonOutput)
 f.close()
 
 cv2.imwrite("generated/imgKeyboard.jpg", imgKeyboard)
-                  
 
 
 cap.release()
